051c44eed34104e5b11e33fa10e07c86feb989a0
yudhi66 <yudhivikram536@gmail.com>
2024-11-02 01:00:32 +0530

    components

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,12 @@
  import Header from "./Header/Header";
  import Footer from "./footer/Footer";
-
+ import Container from "./container/container";
+ import Logo from "./Logo";
+ import LogoutBtn from "./Header/LogoutBtn";
  export{
     Header,
-    Footer
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
  }
--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -14,7 +14,7 @@ const authSlice=createSlice({
             state.userData=action.payload.userData;
         },
         logout:(state)=>{
-            state.state=false;
+            state.status=false;
             state.userData=null;
         }
     }
